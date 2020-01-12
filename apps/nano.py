2cda1095b28b238e22f15ff5b643748846322e44
HATS184 <56793799+HATS184@users.noreply.github.com>
2020-01-12 16:33:55 +0000

    Delete nano.py as it is useless

--- apps/nano.py
+++
@@ -1,9 +0,0 @@
-import PyQt5.QtWidgets as qt
-
-nano = qt.QApplication([])
-frame = qt.QWidget(nano)
-textbox = qt.QLineEdit(frame)
-textbox.move(20, 20)
-textbox.resize(280,40)
-textbox.show()
-frame.show()
