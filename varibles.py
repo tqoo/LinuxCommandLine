5eaec12461430820343abd59d1cbf46185bc3bcb
HATS184 <zanegaffori@gmail.com>
2020-01-18 10:28:20 +0000

    update varibles.py

--- varibles.py
+++ varibles.py
@@ -7,5 +7,5 @@ login = os.getlogin()
 hostname = socket.gethostname()
 currentDirectory = '~'
 start = login + '@' + hostname + ':' + currentDirectory + user + ' '
-home = ['Desktop', 'Downloads', 'Pictures', 'snap', 'Videos', 'Documents', 'Music', 'Public', 'Templates']#
-
+home = ['Desktop', 'Downloads', 'Pictures', 'snap', 'Videos', 'Documents', 'Music', 'Public', 'Templates']
+nanopath = os.getcwd() + '/files/nano.html'
