1d31f52dd826ecd9db098b6b49b0d0e73b62f0de
HATS184 <zanegaffori@gmail.com>
2020-01-18 10:46:51 +0000

    update LinuxCommandLine.py

--- LinuxCommandLine.py
+++ LinuxCommandLine.py
@@ -8,25 +8,28 @@ while True:
     terminputx = ''
     terminput = terminput.replace(' ', '\n')
     termlist = terminput.splitlines()
-    if termlist[0] not in commands:
-        print(termlist[0] + ': command not found')
-    if termlist[0] == 'ls' and currentDirectory == '~':
-        print('''
-        Desktop    Downloads         Pictures  snap       Videos
-    Documents    Music              Public    Templates
-    ''')
-    if termlist[0] == 'nano' and len(termlist) == 2:
-        print('Opening nano. When finished, type \'exit\' and click Enter to exit')
-        webbrowser.open(nanopath)
-    if termlist[0] == 'cd' and len(termlist) == 2:
-        if termlist[1] in home:
-            currentPath += '/' + termlist[1]
-            currentDirectory = termlist[1]
-        if termlist[1] == '..':
-            if currentDirectory in home:
-                currentDirectory = '~'
-                currentPath = '~'
-    start = login + '@' + hostname + ':' + currentPath + user + ' '
+    if terminput == ' ':
+        pass
+    else:
+        if termlist[0] not in commands:
+            print(termlist[0] + ': command not found')
+        if termlist[0] == 'ls' and currentDirectory == '~':
+            print('''
+            Desktop    Downloads         Pictures  snap       Videos
+        Documents    Music              Public    Templates
+        ''')
+        if termlist[0] == 'nano' and len(termlist) == 2:
+            print('Opening nano. When finished, type \'exit\' and click Enter to exit')
+            webbrowser.open(nanopath)
+        if termlist[0] == 'cd' and len(termlist) == 2:
+            if termlist[1] in home:
+                currentPath += '/' + termlist[1]
+                currentDirectory = termlist[1]
+            if termlist[1] == '..':
+                if currentDirectory in home:
+                    currentDirectory = '~'
+                    currentPath = '~'
+        start = login + '@' + hostname + ':' + currentPath + user + ' '
 
     
 
