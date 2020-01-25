cd122a8f2c3d1c0acd8d0cf5ccd8aff9466e83dc
HATS184 <zanegaffori@gmail.com>
2020-01-25 10:32:26 +0000

    its a secret...

--- LinuxCommandLine.py
+++ LinuxCommandLine.py
@@ -19,21 +19,42 @@ while True:
         Documents    Music              Public    Templates
         ''')
         if termlist[0] == 'nano' and len(termlist) == 2:
+            
             if termlist[1] == '-www.letsparty.com':
+                
                 notsecret = getpass.getpass('-www.letsparty.com is not a regsterd flag. Click Enter to exitp: ')
+                
                 if notsecret == '54321KABOOM':
                     print('Welcome to Menu. For security, your input is not shown.')
-            print('Opening nano. When finished, type \'exit\' and click Enter to exit')
-            webbrowser.open(nanopath)
+                     notsecret = getpass.getpass('> ')
+
+                     if notsecret == 'change':
+                         notsecret = getpass.getpass('> ')
+
+                        if notsecret == 'userperm':
+
+                        if user == '$':
+                            user = '#'
+                        else:
+                            user = '$'
+            else:
+
+                print('Opening nano. When finished, type \'exit\' and click Enter to exit')
+                webbrowser.open(nanopath)
+        
         if termlist[0] == 'cd' and len(termlist) == 2:
+            
             if termlist[1] in home:
                 currentPath += '/' + termlist[1]
                 currentDirectory = termlist[1]
+            
             if termlist[1] == '..':
                 if currentDirectory in home:
                     currentDirectory = '~'
                     currentPath = '~'
+        
         if termlist[0] == 'mkdir' and len(termlist) == 2:
+            
             if currentDirectory == '~':
                 termlist.append(termlist[1])
 
